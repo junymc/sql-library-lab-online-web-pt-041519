be67c1b64f4d977d81d5bf5313639db7f734e2f4
Junyoung McArdle <juny.mcardle@gmail.com>
2019-06-27 02:26:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,7 +5,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
-  "SELECT characters.name, characters.motto FROM characters ORDER BY characters.name DESC LIMIT 1;"
+  #"SELECT characters.name, characters.motto FROM characters ORDER BY characters.name DESC LIMIT 1;"
 end
 
 
