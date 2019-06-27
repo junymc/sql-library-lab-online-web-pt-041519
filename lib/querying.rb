024016a4c457e07f4c8d63c79ae8db11d5b29802
Junyoung McArdle <juny.mcardle@gmail.com>
2019-06-27 02:24:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books ORDER BY books.year ASC LIMIT 3;"
+  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY books.year;"
+  #"SELECT books.title, books.year FROM books ORDER BY books.year ASC LIMIT 3;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -8,7 +9,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT value, "
 end
 
 def select_name_and_series_subgenres_of_authors
