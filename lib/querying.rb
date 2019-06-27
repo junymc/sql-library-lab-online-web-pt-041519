8b78dfb00112ba19042d6dbba7217e918d018607
Junyoung McArdle <juny.mcardle@gmail.com>
2019-06-27 02:48:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,8 +21,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM
-  "
+  "SELECT authors.name, subgenres.name FROM "
 end
 
 def select_series_title_with_most_human_characters
@@ -30,10 +29,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, count(name)
+  "SELECT characters.name, COUNT(name)
   FROM character_books
   JOIN characters ON character_books.character_id = characters.id
   GROUP BY characters.name
-  ORDER BY count(name) DESC;
+  ORDER BY COUNT(name) DESC;
   "
 end
